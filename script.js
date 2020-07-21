e8f0b3bdca9af15d820a5d6565c5aa4423d3dcd7
Staffan Småros <staffan.smaros@gmail.com>
2020-07-21 13:39:13 +0300

    Implemented basic score and round counting

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-const choices = ['rock', 'paper', 'scissors'];
+let round = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-function gameScreen(e) {
-    const screen = document.querySelector('.gameplay');
-    let screenMsg = playRound(this.id).msg;
-    screen.textContent = screenMsg;
+function playerWon(selectionInput, computerSelection) {
+    const winLogic = {
+        rock: { win: 'scissors', loose: 'paper' },
+        paper: { win: 'rock', loose: 'scissors' },
+        scissors: { win: 'paper', loose: 'rock'}
+    }
+    return winLogic[selectionInput].win === computerSelection;
 }
 
-const playerSelection = document.querySelectorAll('.selection');
-playerSelection.forEach(selection => selection.addEventListener('click', gameScreen))
-
-// function playerPrompt() {
-//     let selection;
-//     while (true) {
-//         selection = prompt('Rock, paper, scissors?');
-//         selection = selection.toLowerCase();
-//         if (choices.includes(selection)) {
-//             break;
-//         } else {
-//             console.log('Please select rock, paper or scissors!')
-//         }
-//     }
-//     return selection;
-// }
 function playRound(selectionInput) {
+    const choices = ['rock', 'paper', 'scissors'];
     let randomNumber = Math.floor(Math.random() * 3);
     let computerSelection = choices[randomNumber];
+
+    round += 1;
     
     if (selectionInput == computerSelection) {
         return { msg: `It's a draw! You both chose ${ selectionInput }`, playerScore: 0, computerScore: 0 };
@@ -35,14 +27,28 @@ function playRound(selectionInput) {
         return { msg: `You lose! ${ computerSelection[0].toUpperCase() + computerSelection.slice(1) } beats ${ selectionInput }`, playerScore: 0, computerScore: 1 };
     }
 }
-function playerWon(selectionInput, computerSelection) {
-    const winLogic = {
-        rock: { win: 'scissors', loose: 'paper' },
-        paper: { win: 'rock', loose: 'scissors' },
-        scissors: { win: 'paper', loose: 'rock'}
-    }
-    return winLogic[selectionInput].win === computerSelection;
+
+function game(e) {
+    let result = playRound(this.id);
+
+    const screen = document.querySelector('.gameplay');
+    screen.textContent = result.msg;
+
+    const playerScoreScreen = document.querySelector('#player-score');
+    const computerScoreScreen = document.querySelector('#computer-score');
+    playerScore += result.playerScore;
+    computerScore += result.computerScore;
+
+    playerScoreScreen.textContent = `You: ${ playerScore }`;
+    computerScoreScreen.textContent = `Computer: ${ computerScore }`;
+
+    const roundScreen = document.querySelector('#roundbox');
+
+    roundScreen.textContent = `Round: ${ round }`;
 }
+
+const playerSelection = document.querySelectorAll('.selection');
+playerSelection.forEach(selection => selection.addEventListener('click', game))
 // function game() {
 //     let playerScore = 0;
 //     let computerScore = 0;
